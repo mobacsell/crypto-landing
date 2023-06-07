3cf5baa20c3c476f2c055ac088cef2f8b193a375
mobacsell <mobacsell@yandex.ru>
2023-06-07 16:04:30 +0300

    feat: add google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&family=Montserrat:wght@500;600&family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
