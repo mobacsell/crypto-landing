a3c41e0c4772305184df7249447ca083a8f1cf16
mobacsell <mobacsell@yandex.ru>
2023-06-07 18:16:10 +0300

    feat: add Promo comp

---
+++ src/components/Promo/index.tsx
@@ -0,0 +1,68 @@
+import styles from "./Promo.module.css";
+import Image from "next/image";
+
+import arrow from "@assets/Promo/Arrow.svg";
+import bitCoin from "@assets/Promo/BitCoin.svg";
+import candy from "@assets/Promo/Candy.svg";
+import eos from "@assets/Promo/Eos.svg";
+import ethereum from "@assets/Promo/ethereum.svg";
+import sCrypt from "@assets/Promo/sCrypt.svg";
+import tCrypt from "@assets/Promo/tCrypt.svg";
+
+export function Promo() {
+  return (
+    <section className={styles.root}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.header}>
+          Просто о крипте
+          <br />
+          на <span className={styles.headerGradient}>фантиках</span>
+        </h1>
+        <p className={styles.description}>
+          Наконец, <span className={styles.light}>все знания</span> собраны в{" "}
+          <span className={styles.light}>одном месте</span>
+          <br />
+          <span className={styles.light}>Простым языком</span>
+        </p>
+        <ul className={styles.list}>
+          <li>
+            Как все работает и{" "}
+            <span className={styles.light}>что такое крипта</span>
+          </li>
+          <li>
+            Зачем нужен <span className={styles.light}>кошелек</span>
+          </li>
+          <li>
+            Почему на этом{" "}
+            <span className={styles.purple}>зарабатывают большие деньги</span>
+          </li>
+        </ul>
+        <button className={styles.button}>Скачать гайд</button>
+        <div className={styles.arrow}>
+          <Image src={arrow} alt="стрелка" />
+        </div>
+        <div className={styles.bitCoin}>
+          <Image src={bitCoin} alt="Биткоин" />
+        </div>
+        <div className={styles.candy}>
+          <Image src={candy} alt="Конфета" />
+        </div>
+        <div className={styles.eos}>
+          <Image src={eos} alt="eos" />
+        </div>
+        <div className={styles.ethereum}>
+          <Image src={ethereum} alt="ethereum" />
+        </div>
+        <div className={styles.sCrypt}>
+          <Image src={sCrypt} alt="ethereum" />
+        </div>
+        <div className={styles.tCrypt}>
+          <Image src={tCrypt} alt="ethereum" />
+        </div>
+        <div className={styles.bitCoinBlur}>
+          <Image src={bitCoin} alt="Биткоин" />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Promo } from "@/components/Promo";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    <main>
+      <Promo />
+    </main>
   );
 }
