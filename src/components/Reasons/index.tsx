82576d831f5f159fdfba17442f0dcf98d7640806
mobacsell <mobacsell@yandex.ru>
2023-06-16 17:27:48 +0300

    feat: add adaptivity to Reasons comp

--- src/components/Reasons/index.tsx
+++ src/components/Reasons/index.tsx
@@ -14,18 +14,18 @@ export function Reasons() {
             <h3 className={styles.contentHeader}>Правильный план объяснения</h3>
             <p className={styles.description}>
               Максимально эффективный путь понимания
-              <br />
+              <br className={styles.br} />
               сферы. Я сам лично прошел несколько курсов
-              <br /> и просмотрел видео. Все они построены не
-              <br /> совсем верно.
+              <br className={styles.br} /> и просмотрел видео. Все они построены не
+              <br className={styles.br} /> совсем верно.
             </p>
           </li>
           <li>
             <h3 className={styles.contentHeader}>Только то, что нужно</h3>
             <p className={styles.description}>
               Без воды. Повторения только там, где сложно
-              <br /> и рисунки, чтобы лучше и более полно
-              <br /> осталось в памяти.
+              <br className={styles.br} /> и рисунки, чтобы лучше и более полно
+              <br className={styles.br} /> осталось в памяти.
             </p>
           </li>
           <li>
@@ -34,17 +34,17 @@ export function Reasons() {
             </h3>
             <p className={styles.description}>
               Помимо технических терминов, в гайде
-              <br /> проводятся аналогии с реальными
-              <br /> предметами. Так легче запоминать.
+              <br className={styles.br} /> проводятся аналогии с реальными
+              <br className={styles.br} /> предметами. Так легче запоминать.
             </p>
           </li>
           <li>
             <h3 className={styles.contentHeader}>Всё в одном месте</h3>
             <p className={styles.description}>
               Ура! Никуда больше не надо ходить, что-либо
-              <br /> искать. Когда я сам начинал изучать эту тему,
-              <br /> то самое сложное было - собрать всё в одном
-              <br /> месте. Здесь же прочитать гайд - и всё.
+              <br className={styles.br} /> искать. Когда я сам начинал изучать эту тему,
+              <br className={styles.br} /> то самое сложное было - собрать всё в одном
+              <br className={styles.br} /> месте. Здесь же прочитать гайд - и всё.
             </p>
           </li>
         </ul>
