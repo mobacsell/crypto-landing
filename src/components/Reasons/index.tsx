04ee0c410c03f76a1209ca2f49b7895f082cfa64
mobacsell <mobacsell@yandex.ru>
2023-07-05 12:54:59 +0300

    feat: Reasons comp - put the data of the list items in the constant

--- src/components/Reasons/index.tsx
+++ src/components/Reasons/index.tsx
@@ -1,6 +1,53 @@
 import styles from "./Reasons.module.css";
 import { Button } from "@/shared/Button";
 
+const list = [
+  {
+    itemHeader: "Правильный план объяснения",
+    itemText: [
+      "Максимально эффективный путь понимания",
+      <br className={styles.br} key="1" />,
+      "сферы. Я сам лично прошел несколько курсов",
+      <br className={styles.br} key="2" />,
+      " и просмотрел видео. Все они построены не",
+      <br className={styles.br} key="3" />,
+      " совсем верно.",
+    ],
+  },
+  {
+    itemHeader: "Только то, что нужно",
+    itemText: [
+      "Без воды. Повторения только там, где сложно",
+      <br className={styles.br} key="1" />,
+      " и рисунки, чтобы лучше и более полно",
+      <br className={styles.br} key="2" />,
+      " осталось в памяти.",
+    ],
+  },
+  {
+    itemHeader: "Простыми словами, на аналогиях",
+    itemText: [
+      "Помимо технических терминов, в гайде",
+      <br className={styles.br} key="1" />,
+      " проводятся аналогии с реальными",
+      <br className={styles.br} key="2" />,
+      " предметами. Так легче запоминать.",
+    ],
+  },
+  {
+    itemHeader: "Всё в одном месте",
+    itemText: [
+      "Ура! Никуда больше не надо ходить, что-либо",
+      <br className={styles.br} key="1" />,
+      " искать. Когда я сам начинал изучать эту тему,",
+      <br className={styles.br} key="2" />,
+      " то самое сложное было - собрать всё в одном",
+      <br className={styles.br} key="3" />,
+      " месте. Здесь же прочитать гайд - и всё.",
+    ],
+  },
+];
+
 export function Reasons() {
   return (
     <section className={styles.root}>
@@ -10,43 +57,14 @@ export function Reasons() {
       </h2>
       <div className={styles.content}>
         <ul className={styles.list}>
-          <li>
-            <h3 className={styles.contentHeader}>Правильный план объяснения</h3>
-            <p className={styles.description}>
-              Максимально эффективный путь понимания
-              <br className={styles.br} />
-              сферы. Я сам лично прошел несколько курсов
-              <br className={styles.br} /> и просмотрел видео. Все они построены не
-              <br className={styles.br} /> совсем верно.
-            </p>
-          </li>
-          <li>
-            <h3 className={styles.contentHeader}>Только то, что нужно</h3>
-            <p className={styles.description}>
-              Без воды. Повторения только там, где сложно
-              <br className={styles.br} /> и рисунки, чтобы лучше и более полно
-              <br className={styles.br} /> осталось в памяти.
-            </p>
-          </li>
-          <li>
-            <h3 className={styles.contentHeader}>
-              Простыми словами, на аналогиях
-            </h3>
-            <p className={styles.description}>
-              Помимо технических терминов, в гайде
-              <br className={styles.br} /> проводятся аналогии с реальными
-              <br className={styles.br} /> предметами. Так легче запоминать.
-            </p>
-          </li>
-          <li>
-            <h3 className={styles.contentHeader}>Всё в одном месте</h3>
-            <p className={styles.description}>
-              Ура! Никуда больше не надо ходить, что-либо
-              <br className={styles.br} /> искать. Когда я сам начинал изучать эту тему,
-              <br className={styles.br} /> то самое сложное было - собрать всё в одном
-              <br className={styles.br} /> месте. Здесь же прочитать гайд - и всё.
-            </p>
-          </li>
+          {list.map((value, index) => {
+            return (
+              <li key={index}>
+                <h3 className={styles.contentHeader}>{value.itemHeader}</h3>
+                <p className={styles.description}>{value.itemText}</p>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <Button
