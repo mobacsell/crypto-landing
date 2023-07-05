adcc43246a791cfdd4ef7e6e6afd4d0b0976099e
mobacsell <mobacsell@yandex.ru>
2023-07-05 10:53:38 +0300

    feat: Information comp - put the data of the list items in the constant

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -5,45 +5,54 @@ import blockchain from "@assets/Information/blockchain.svg";
 import tokens from "@assets/Information/tokens.svg";
 import wallet from "@assets/Information/wallet.svg";
 
+const list = [
+  {
+    imagePath: tokens,
+    imageName: "Логотип эфириум",
+    header: "Крипта и токены",
+    description: [
+      "Что это такое",
+      <br key="1" />,
+      "Почему на них зарабатывают",
+    ],
+  },
+  {
+    imagePath: blockchain,
+    imageName: "Логотип блокчейн",
+    header: "Блокчейн",
+    description: [
+      "Что это такое",
+      <br key="1" />,
+      "Почему на них зарабатывают",
+    ],
+  },
+  {
+    imagePath: wallet,
+    imageName: "Кошелек",
+    header: "Кошелек",
+    description: ["Для чего он нужен", <br key="2" />, "Можно ли без него"],
+  },
+];
+
 export function Infromation() {
   return (
     <section className={styles.root}>
       <div className={styles.wrapper}>
         <h2 className={styles.header}>Вы узнаете</h2>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <Image src={tokens} alt="Крипта и токены" />
-            <div className={styles.content}>
-              <h3 className={styles.contentHeader}>Крипта и токены</h3>
-              <p className={styles.contentDescription}>
-                Что это такое
-                <br />
-                Почему на них зарабатывают
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <Image src={blockchain} alt="Блокчейн" />
-            <div className={styles.content}>
-              <h3 className={styles.contentHeader}>Блокчейн</h3>
-              <p className={styles.contentDescription}>
-                Как он устроен
-                <br />
-                Почему он безопасен
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <Image src={wallet} alt="Кошелек" />
-            <div className={styles.content}>
-              <h3 className={styles.contentHeader}>Кошелек</h3>
-              <p className={styles.contentDescription}>
-                Для чего он нужен
-                <br />
-                Можно ли без него
-              </p>
-            </div>
-          </li>
+          {list.map((value, index) => {
+            return (
+              <li className={styles.item} key={index}>
+                <Image src={value.imagePath} alt={value.imageName} />
+                <div className={styles.content}>
+                  <h3 className={styles.contentHeader}>{value.header}</h3>
+                  <p className={styles.contentDescription}>
+                    {value.description}
+                  </p>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </section>
