ade98b04335b77afc3c3f6222549cbea872aeecf
mobacsell <mobacsell@yandex.ru>
2023-07-18 18:48:49 +0300

    feat: add title to landing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,13 @@ export default function Document() {
   return (
     <Html lang="ru">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&family=Montserrat:wght@500;600&family=Roboto:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
+        <>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&family=Montserrat:wght@500;600&family=Roboto:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
+          <title>Просто о крипте на фантиках</title>
+        </>
       </Head>
       <body>
         <Main />
